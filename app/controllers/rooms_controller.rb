c495f9565d3b721ebb32d5cf9b61161122faef27
gmgn120 <gmgn120@gmail.com>
2021-01-28 07:14:03 +0000

    Fix rooms_controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,17 +3,17 @@ class RoomsController < ApplicationController
   
   def create
     @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    @user_room1 = UserRoom.create(room_id: @room.id, user_id: current_user.id)
+    @user_room2 = UserRoom.create(params.require(:user_room).permit(:user_id, :room_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
   end
   
   def show
     @room = Room.find(params[:id])
-    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
-      @messages = @room.messages
-      @message = Message.new
-      @entries = @room.entries
+    if UserRoom.where(user_id: current_user.id, room_id: @room.id).present?
+      @chats = @room.chats
+      @chat = Chat.new
+      @user_rooms = @room.user_room
     else
       redirect_back(fallback_location: root_path)
     end
