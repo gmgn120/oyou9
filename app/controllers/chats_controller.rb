93229ac234ba7e9d72bfa08dac46a43547b69869
gmgn120 <gmgn120@gmail.com>
2021-01-29 05:46:19 +0000

    Finish oyou9

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,14 @@
 class ChatsController < ApplicationController
-  
+
   before_action :authenticate_user!, only: [:create]
-  
+
+  def create
+    if UserRoom.where(user_id: current_user.id, room_id: params[:chat][:room_id]).present?
+      @chat = Chat.create(params.require(:chat).permit(:user_id, :chat, :room_id).merge(user_id: current_user.id))
+    else
+      flash[:alert] = "メッセージ送信に失敗しました。"
+    end
+    redirect_to "/rooms/#{@chat.room_id}"
+  end
+
 end
